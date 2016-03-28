d42a2e4d268291e58ac760bdaed8857f55dd588e
Kris Stange <kris.stange2015@gmail.com>
2016-03-28 00:23:28 -0700

    passing all tests exercises/bare_minimum/promiseConstructor.js

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -39,10 +39,17 @@ var pluckFirstLineFromFileAsync = function(filePath) {
   });
 };
 
-
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function (url) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    request.get(url, function(err, response) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can unit test them
